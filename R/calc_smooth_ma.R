441b854f0df9a72331bdcf9f81d914d7a25f0935
KwokJunKin <102900899+CarolusKwok@users.noreply.github.com>
2023-03-11 20:59:40 +0800

    v2.5
    Calc_smooth_na performance increase

--- R/calc_smooth_ma.R
+++ R/calc_smooth_ma.R
@@ -3,14 +3,16 @@
 #' @param data The Data frame
 #' @param based A column within dataframe. This column must be "numeric-able", must not contain NAs, and must be unique
 #' @param value A column within data
-#' @param side Accepts 1 string of "left", "center", or "right"
-#' @param weight weighting or the size of the window
+#' @param type Accepts 1 string of "left", "center", or "right"
+#' @param weight Weighting or the size of the window
+#' @param name_as Name of the new smoothed column. Default as predict_xxx, where xxx is the column name of the based.
+#' @param NAs Removes NA in the smoothed column by treating missing data
 #'
 #' @return
 #' @export
 #'
-#' @examples calc_smooth_ma(data, x, y)
-calc_smooth_ma = function(data, based, value, side = "center", weight = 3){
+#' @examples
+calc_smooth_ma = function(data, based, value, type = "center", weight = 3, name_as = "", NAs = T){
   #Check ####
   if(weather2::w2_check_type_dataframe(data = data, data_name = "data")){return(invisible())}
   if(weather2::w2_check_col_exist(data = data, data_name = "data", value = {{based}}, value_name = "based")){return(invisible())}
@@ -18,69 +20,79 @@ calc_smooth_ma = function(data, based, value, side = "center", weight = 3){
   if(weather2::w2_check_type_numeric(value = weight, value_name = "weight")){return(invisible())}
 
   data0 = dplyr::select(data, x = {{based}})$x
-  if(weather2::w2_check_list_na(data = data0, data_name = "based")){return(invisible())}
+  if(weather2::w2_check_list_na(data = data0, data_name = "based", NAs = F)){return(invisible())}
   if(weather2::w2_check_list_numericable(data = data0, data_name = "based")){return(invisible())}
   if(weather2::w2_check_list_unique(data = data0, data_name = "based")){return(invisible())}
 
   #arrange the data by based, grab based as x and value as y, mutate to have a list of na ####
   data = dplyr::arrange(data, {{based}})
-  data0 = dplyr::select(data, x = {{based}}, y = {{value}}) %>%
-    dplyr::mutate(predict = NA_real_)
+  data0 = dplyr::select(data, x = {{based}}, y = {{value}})
+  #calculate weight and get the sides ####
+  if(length(weight) == 1){weight = rep(1/weight, weight)}
+  weight = weight[length(weight):1]
 
-  #calculate n and weight ####
-  if(length(weight) == 1){
-    n = weight
-    weight = rep(1/n, n)
-  }
-  if(length(weight) != 1){
-    n = length(weight)
-  }
-  #start calculating ####
-  if(side == "left"){
-    for(i in 1:nrow(data0)){
-      start = i
-      end = i+n-1
-      ls = tibble::tibble(value  = data0$y[start:end],
-                          weight = weight,
-                          new    = value * weight)
-      sum = sum(ls$new, na.rm = T)
-      data0$predict[i] = sum
-    }
-  }
-  if(side == "right"){
+  if(type == "center"){
+    sides = 2
+  } else if (type == "top") {
+    sides = 1
+    weight = weight[length(weight):1]
     data0 = dplyr::arrange(data0, dplyr::desc(x))
-    for(i in 1:nrow(data0)){
-      start = i
-      end = i+n-1
-      ls = tibble::tibble(value  = data0$y[start:end],
-                          weight = weight,
-                          new    = value * weight)
-      sum = sum(ls$new, na.rm = T)
-      data0$predict[i] = sum
-    }
-    data0 = dplyr::arrange(data0, x)
-  }
-  if(side == "center"){
-    left = floor((n-1)/2)
-    right= floor(n/2)
-    for(i in 1:nrow(data0)){
-      start = ifelse(i-left <=0, 1, i-left)
-      end = i+right
-      ls = data0$y[start: end]
-      if(length(ls) != n){
-        ls = c(rep(NA_real_, n - length(ls)), ls)
-      }
-      ls = tibble::tibble(value  = ls,
-                          weight = weight,
-                          new    = value * weight)
-      sum = sum(ls$new, na.rm = T)
-      data0$predict[i] = sum
-    }
+  } else if (type == "bottom"){
+    sides = 1
   }
 
+  #start calculating ####
+  if(NAs == T){
+    prediction = as.numeric(stats::filter(x = data0$y, filter = weight, method = "convolution", sides = sides))
+    data0 = dplyr::mutate(data0, predict = prediction)
+  }
+  if(NAs == F){
+    data0 = dplyr::bind_rows(tibble::tibble(x = NA, y = NA, predict = NA, .rows = length(weight)),
+                             data0) %>%
+      dplyr::bind_rows(tibble::tibble(x = NA, y = NA, predict = NA, .rows = length(weight)))
+    prediction = as.numeric(stats::filter(x = data0$y, filter = weight, method = "convolution", sides = sides))
+
+    weight = weight[length(weight):1]
+    data0 = dplyr::mutate(data0,
+                          predict = prediction,
+                          row  = 1:dplyr::n())
+    if(type == "center"){
+      if((length(weight) %% 2) == 1){
+        data0 = dplyr::mutate(data0,
+                              start = row - floor(length(weight)/2),
+                              end   = row + floor(length(weight)/2),
+                              run = !is.na(x) & is.na(predict))
+      }
+      if((length(weight) %% 2) == 0){
+        data0 = dplyr::mutate(data0,
+                              start = row - (length(weight)/2 - 1),
+                              end = row + (length(weight)/2),
+                              run = !is.na(x) & is.na(predict))
+      }
+    }
+    if(type == "top" | type == "bottom"){
+      data0 = dplyr::mutate(data0,
+                            start = row - length(weight) + 1,
+                            end = row,
+                            run = !is.na(x) & is.na(predict))
+    }
+    list_run = dplyr::filter(data0, run == T)$row
+    for(i in list_run){
+      start = data0$start[i]
+      end   = data0$end[i]
+      reana = data0[start:end, "y"] %>%
+        dplyr::mutate(w = weight,
+                      s = y*w)
+      sum   = sum(reana$s, na.rm = T)
+      data0$predict[i] = sum
+    }
+    data0 = dplyr::filter(data0,
+                          !is.na(x) & !is.na(y) & !is.na(predict))
+  }
+  data0 = dplyr::arrange(data0, x)
   #return the data ####
-  name_value = colnames(dplyr::select(data, {{value}}))
-  expected_colname = weather2::w2_get_colname(data, name = paste0("predict_", name_value))
+  if(name_as == ""){name_as = paste0("predict_", colnames(dplyr::select(data, {{value}})))}
+  expected_colname = weather2::w2_get_colname(data, name = name_as)
   data = dplyr::mutate(data, "{expected_colname}" := data0$predict)
   return(data)
 }
